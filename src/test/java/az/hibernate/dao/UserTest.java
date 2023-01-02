f3d72a8094e79f14c021c79ddc6a31eec7d024f9
iamusayev <anarmusayev1337@gmail.com>
2023-01-02 15:49:20 +0400

    added @Fetch(FetchMode.SUBSELECT)

--- src/test/java/az/hibernate/dao/UserTest.java
+++ src/test/java/az/hibernate/dao/UserTest.java
@@ -4,6 +4,8 @@ import az.hibernate.repeat.dao.UserDao;
 import az.hibernate.repeat.entity.User;
 import az.hibernate.repeat.util.HibernateUtil;
 import java.util.List;
+import lombok.Cleanup;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,17 @@ class UserTest {
     void batchSize() {
         List<User> users = userDao.findAllByCompanyName(sessionFactory.openSession(), "Google");
         users.forEach(user -> user.getPayments().forEach(System.out::println));
+    }
+
+    @Test
+    void fetchAnnotation() {
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<User> users = session.createQuery("select u from User u ", User.class).list();
+        users.forEach(user -> System.out.println(user.getPayments().size()));
+        users.forEach(user -> System.out.println(user.getCompany().getName()));
+
+        session.getTransaction().commit();
     }
 }
