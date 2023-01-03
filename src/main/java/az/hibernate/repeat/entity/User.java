ea42a650dd148da5fa032589c973510e07d739c4
iamusayev <anarmusayev1337@gmail.com>
2023-01-03 17:05:26 +0400

    added query fetch in HQL, Criteria API and Querydsl

--- src/main/java/az/hibernate/repeat/entity/User.java
+++ src/main/java/az/hibernate/repeat/entity/User.java
@@ -1,18 +1,16 @@
 package az.hibernate.repeat.entity;
 
-import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 
 import az.hibernate.repeat.model.Role;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -22,11 +20,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.BatchSize;
+
 
 @Data
-@EqualsAndHashCode(exclude = {"company", "payments"})
-@ToString(exclude = {"company", "payments"})
+@EqualsAndHashCode(exclude = "company")
+@ToString(exclude = "company")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -38,18 +36,11 @@ public class User {
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
     private String username;
-    private String info;
-    private PersonalInfo personalInfo;
-
-    @ManyToOne(fetch = LAZY, cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "company_id")
+    @ManyToOne(fetch = FetchType.LAZY)
     private Company company;
-
+    private PersonalInfo personalInfo;
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    @Builder.Default
-    @OneToMany(mappedBy = "receiver")
-    @BatchSize(size = 3)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "receiver")
     private List<Payment> payments = new ArrayList<>();
 }
