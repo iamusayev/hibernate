9454710a29c77ed77b0df402d13189835b6fcc3a
iamusayev <anarmusayev1337@gmail.com>
2023-01-06 19:40:48 +0400

    added optimistic lock with version

--- src/test/java/HibernateRunner.java
+++ src/test/java/HibernateRunner.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class HibernateRunner {
+
     public static void main(String[] args) {
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
              Session session = sessionFactory.openSession();
