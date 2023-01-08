2c495fb060a12c87c2e7c51ac020b84ffeca2f56
iamusayev <anarmusayev1337@gmail.com>
2023-01-08 16:51:25 +0400

    added entity callbacks with listener

--- src/main/java/az/hibernate/repeat/entity/Payment.java
+++ src/main/java/az/hibernate/repeat/entity/Payment.java
@@ -1,13 +1,13 @@
 package az.hibernate.repeat.entity;
 
- import java.time.Instant;
+import az.hibernate.listener.AuditableListener;
+import java.time.Instant;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
 import javax.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,8 +15,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.OptimisticLockType;
-import org.hibernate.annotations.OptimisticLocking;
 
 @Data
 @EqualsAndHashCode(exclude = "receiver")
@@ -25,28 +23,23 @@ import org.hibernate.annotations.OptimisticLocking;
 @AllArgsConstructor
 @Builder
 @Entity
-@OptimisticLocking(type = OptimisticLockType.VERSION)
+@EntityListeners(value = AuditableListener.class)
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private Integer amount;
 
     @Version
     private Long version;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "receiver_id")
     private User receiver;
 
     private Instant createdAt;
     private String createdBy;
-
-    @PrePersist
-    public void prePersist() {
-         setCreatedAt(Instant.now());
-        setCreatedBy("Security Context");
-    }
-
+    private Instant updatedAt;
+    private String updatedBy;
 }
